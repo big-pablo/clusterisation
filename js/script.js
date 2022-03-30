ac7d85277f8fa9130b032ec34bd0baa0874eb7a5
big-pablo <fckem.nggaz@gmail.com>
2022-03-30 17:48:26 +0700

    Почти рабочая кластеризация
    Алгос поправить бы, потому что сейчас: а) не хватает итераций б) всё зависит от изначально рандомно выбранных центроид
    Скорее всего второе
    Типа:
    Строю 3 тройки точек
    Центроиды выбираются как две точки из изначального кластера и одна из третьего
    Одна из центроид загребает свой кластер и ещё тот, который рядом
    А из-за выбора центроид в UpdateCentroids, кластер, поглощённый другим кластером, не может провозгласить свою центроиду и уйти к ней (ЛДНР какая-то получается)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 var points = new Array();
 var clonepoints = new Array();
+var centroids = new Array();
 class Point {
     constructor(options) {
         this.x = options.x;
         this.y = options.y;
         this.num = options.num;
+        this.disttocentroid = options.disttocentroid;
     }
 }
 function draw()
@@ -25,7 +27,7 @@ function draw()
           num: count
       });
       points[count-1] = currentpoint;
-      clonepoints[count-1] = currentpoint;
+      clonepoints.push(currentpoint);
       document.getElementById("amount").max = count;
       count++;
       console.log(currentpoint)
@@ -39,37 +41,59 @@ function changelabel()
 
 function clusters()
 {
-    var centroidsamount = parseInt(document.getElementById("amount").value);
-    var centroids = new Array();
-   // var clonepoints = points.slice(0);
     var clusters = new Array();
-    for (var i = 0; i < centroidsamount;i++)
+    //Выбираю центроиды
+    for (let i = 0; i<parseInt(document.getElementById("amount").value);i++)
     {
         var cluster = new Array();
-        let currentrandom = getRandomIntInclusive(0,clonepoints.length-1);
-        centroids[i] = clonepoints[currentrandom];
-        clonepoints.splice(currentrandom,1);
-        cluster.push(centroids[i]);
-        clusters.push(cluster);
-        console.log("Центроиды:")
-        console.log(centroids[i]);
+        let randomindex = getRandomIntInclusive(0,clonepoints.length-1);
+        centroids.push(clonepoints[randomindex]);
+        cluster.push(clonepoints[randomindex]);
+        clonepoints.splice(randomindex,1);
+        clusters[i] = cluster;
     }
     for (var i = 0; i<clonepoints.length;i++)
     {
-        var current = clonepoints[i];
-        var mindist = 1500;
+        var mindist = 1500; //Здесь 1500 потому что канвас 1000х1000
+        current = clonepoints[i];
         for (var k = 0; k<centroids.length;k++)
         {
-            if (mindist < distance(current[1],centroids[k][1],current[2],centroids[k][2]))
+            if (distance(centroids[k].x, current.x, centroids[k].y, current.y) < mindist)
             {
-                mindist = distance(current[1],centroids[k][1],current[2],centroids[k][2]);
-                var wheremin = k;
+                mindist = distance(centroids[k].x, current.x, centroids[k].y, current.y);
+                var whereto = k;
             }
         }
-        clusters[wheremin].push(current);
-    }
-    console.log(centroids);
+        clusters[whereto].push(clonepoints[i]);
+    } 
+    //updateCentroids(clusters, centroids)
+    
+    for (let f = 0; f<100; f++)
+    {
+       updateCentroids(clusters,centroids); //Обновляю центроиды
+       clusters.splice(0,clusters.length);  //Очищаю кластеры
+       for (let i = 0; i<parseInt(document.getElementById("amount").value);i++) //Очищаю кластеры, вроде норм
+       {
+           var internalcluster = Array();
+           clusters.push(internalcluster);
+       }
+       for (let i = 0; i<points.length;i++) //Прохожусь по всем точкам
+       {
+        var mindist = 1500; //Здесь 1500 потому что канвас 1000х1000
+        current = points[i];
+        for (var k = 0; k<centroids.length;k++)
+        {
+            if (distance(centroids[k].x, current.x, centroids[k].y, current.y) <= mindist)
+            {
+                mindist = distance(centroids[k].x, current.x, centroids[k].y, current.y);
+                whereto = k;
+            }
+        }
+        clusters[whereto].push(current);
+       } 
+    } 
     console.log(clusters);
+    console.log(centroids)
 }
 
 function updateCentroids(clusters, centroids)
@@ -77,15 +101,27 @@ function updateCentroids(clusters, centroids)
     centroids.splice(0,centroids.length);
     for (let i = 0; i<clusters.length;i++)
     {
-        let sumdistx = 0;
-        let sumdisty = 0;
-        for (let k = 0; k <clusters[i].length;k++)
+        var newcentroidindex = 0;
+        var sumdistx = 0;
+        var sumdisty = 0;
+        for (let k = 0; k < clusters[i].length;k++)
         {
             sumdistx += clusters[i][1];
             sumdisty += clusters[i][2];
         }
         midx = sumdistx / clusters[i].length;
         midy = sumdisty / clusters[i].length;
+        var mindistance = 1500;
+        for (let k = 0; k<clusters[i].length;k++)
+        {
+            if (distance(clusters[i][k].x, midx, clusters[i][k].y, midy) < mindistance)
+            {
+                mindistance = distance(clusters[i][k].x, midx, clusters[k][k].y, midy);
+                newcentroidindex = k; 
+            }
+        }
+        centroids.push(clusters[i][newcentroidindex]);
+        //clusters[i].splice(newcentroidindex,1);
     }
 }
 
